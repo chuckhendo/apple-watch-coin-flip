f98316356edb89a1f9c8cff34eb980e51e6ecbf2
Anthony Henderson <chuckhendo@gmail.com>
2016-06-23 17:00:57 -0400

    Basic functionality

--- CoinFlip WatchKit Extension/InterfaceController.swift
+++ CoinFlip WatchKit Extension/InterfaceController.swift
@@ -11,6 +11,7 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    @IBOutlet var button: WKInterfaceButton!
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
@@ -26,6 +27,25 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+    
+    
+    @IBAction func flipCoin() {
+        button.setTitle("Flipping...")
+        let randomNum = arc4random_uniform(2)
+        let result = randomNum == 1 ? "Heads" : "Tails"
+        delay(0.3) {
+            self.button.setTitle(result)
+        }
     }
 
 }
